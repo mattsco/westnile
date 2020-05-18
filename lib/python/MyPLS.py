61ac286b6098e2aa3d22fe2602ea26f624bb301d
admin <matt@dataiku.com>
2020-05-18 11:11:48 +0800

    edit files

--- lib/python/MyPLS.py
+++ lib/python/MyPLS.py
@@ -3,7 +3,7 @@ from sklearn.cross_decomposition import PLSRegression
 class MyPLS():
     def __init__(self, n_components=2, scale=True, max_iter=500, tol=1e-06, copy=True):
         self.pls = PLSRegression(n_components, scale, max_iter, tol, copy)
-        self.classes_ = []
+        self.classes_ = 3
         
     def fit(self, X, Y):
         self.pls.fit(X, Y)
